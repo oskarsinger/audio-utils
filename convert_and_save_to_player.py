b914291f73a9a1c3905edffcaf278cc39d42ca2b
oskar <oskarsinger@gmail.com>
2018-10-11 18:44:45 -0500

    Fixed index and filetype bugs

--- convert_and_save_to_player.py
+++ convert_and_save_to_player.py
@@ -1,14 +1,16 @@
 import click
+import shutil
 
 from pydub import AudioSegment
 from os import listdir, mkdir
 from os.path import isfile, join, splitext, exists
 
 @click.command()
-@click.option('-s')
-@click.option('-t')
-@click.option('-f', default='mp3')
-@click.option('-b', default='128k')
+@click.option('--source')
+@click.option('--target')
+@click.option('--target-format', default='mp3')
+@click.option('--bitrate', default='128k')
+@click.option('--copy-non-sound', default=False)
 def run_tings_all_day_bb(
     source,
     target,
@@ -32,23 +34,34 @@ def run_tings_all_day_bb(
 
         source_song_path = join(source, sfn)
         (name, source_format) = splitext(sfn)
-        target_fn = name + '.' + target_format
-        target_song_path = join(album_dir, target_fn)
+        source_format = source_format[1:]
 
-        print('Importing file:', source_song_path)
+        if source_format in {'mp3', 'flac', 'wav', 'ogg'}:
+            target_fn = name + '.' + target_format
+            target_song_path = join(album_dir, target_fn)
 
-        song = AudioSegment.from_file(
-            source_song_path, 
-            format=source_format)
+            print('Importing file:', source_song_path)
 
-        print('Exporting file:', target_song_path)
-        
-        song.export(
-            target_song_path, 
-            format=target_format,
-            bitrate=bitrate)
+            song = AudioSegment.from_file(
+                source_song_path, 
+                format=source_format)
 
-        print()
+            print('Exporting file:', target_song_path)
+            
+            song.export(
+                target_song_path, 
+                format=target_format,
+                bitrate=bitrate)
+
+            print()
+        elif copy_non_sound:
+            target_file_path = join(album_dir, sfn)
+            source_file_path = join(source, sfn)
+            
+            shutil.copyfile(
+                source_file_path,
+                target_file_path)
+
 
 if __name__=='__main__':
     run_tings_all_day_bb()
