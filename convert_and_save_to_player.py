2267b46560e848e4e4a64d72378378f277b077c5
oskar <oskarsinger@gmail.com>
2018-10-26 00:08:15 -0500

    Can parallelize by song now

--- convert_and_save_to_player.py
+++ convert_and_save_to_player.py
@@ -1,8 +1,5 @@
 import click
-import shutil
 
-from os import listdir
-from os.path import isfile, join
 from audioutils import *
 
 @click.command()
@@ -11,36 +8,25 @@ from audioutils import *
 @click.option('--target-format', default='mp3')
 @click.option('--bitrate', default='128k')
 @click.option('--copy-non-sound', default=False)
+@click.option('--num-processes', default=1)
 def run_tings_all_day_bb(
     source,
     target,
     target_format,
     bitrate,
-    copy_non_sound):
+    copy_non_sound,
+    num_processes):
 
     (artist_dir, album_dir) = get_and_make_artist_and_album_dirs(
         source,
         target)
-    c_and_w_song = lambda sfn: convert_and_write_song(
-        sfn,
+    convert_album(
         album_dir,
         target_format,
         source,
-        bitrate)
-    song_filenames = [fn for fn in listdir(source)
-                      if isfile(join(source, fn))]
-
-    for sfn in song_filenames:
-        source_format = get_name_and_format(sfn)[1]
-        if source_format in {'mp3', 'flac', 'wav', 'mp4'}:
-            c_and_w_song(sfn)
-        elif copy_non_sound:
-            target_file_path = join(album_dir, sfn)
-            source_file_path = join(source, sfn)
-            
-            shutil.copyfile(
-                source_file_path,
-                target_file_path)
+        bitrate,
+        copy_non_sound,
+        num_processes)
 
 if __name__=='__main__':
     run_tings_all_day_bb()
