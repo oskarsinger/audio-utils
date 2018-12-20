e81b6c2d1028a2d3b6682f65f7d5a5c79779f591
Oskar Singer <oskarsinger@gmail.com>
2018-12-20 00:13:56 -0500

    Toward safer Dropbox interactions

--- audioutils/db/tables.py
+++ audioutils/db/tables.py
@@ -1,39 +1,62 @@
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
 
-class IncompleteDropboxDownload(Base):
+class Incomplete(Base):
 
-    __tablename__ = 'incomplete_dropbox_download'
+    __tablename__ = 'incomplete'
 
-    dbx_path = Column(String, primary_key=True)
+    path = Column(String, primary_key=True)
+    insertion_time = Column(DateTime)
+
+class Failed(Base):
+
+    __tablename__ 'failed'
+
+    path = Column(String, primary_key=True)
+    error_message = Column(String)
     insertion_time = Column(DateTime)
 
 
-class FailedDropboxUpload(Base):
+class IncompleteDropboxUpload(Incomplete):
+
+    __tablename__ = 'incomplete_dropbox_upload'
+
+
+class IncompleteDropboxDownload(Incomplete):
+
+    __table__ = 'incomplete_dropbox_download'
+
+
+class IncompleteMP3Transfer(Incomplete):
+
+    __table__ = 'incomplete_mp3_transfer'
+
+
+class FailedDropboxDownload(Failed):
+
+    __tablename__ = 'failed_dropbox_download'
+
+
+class FailedDropboxUpload(Failed):
 
     __tablename__ = 'failed_dropbox_upload'
 
-    local_path = Column(String, primary_key=True)
-    insertion_time = Column(DateTime)
 
+class FailedMP3Transfer(Failed):
 
-class IncompleteMP3Transfer(Base):
-
-    __tablename__ = 'incomplete_mp3_transfer'
-
-    local_path = Column(String, primary_key=True)
-    insertion_time = Column(DateTime)
+    __tablename__ = 'failed_mp3_transfer'
 
 
 class TooLargeDropboxUpload(Base):
 
     __tablename__ = 'too_large_dropbox_upload'
 
-    dbx_path = Column(String, primary_key=True)
+    path = Column(String, primary_key=True)
+    size = Column(Integer)
     insertion_time = Column(DateTime)
 
 
@@ -42,12 +65,9 @@ class Registry(Base):
     __table__ = 'registry'
 
     local_path = Column(String, primary_key=True)
-    insertion_time = Column(DateTime)
-
-
-class ToDropboxUpload(Base):
-
-    __table__ = 'to_dropbox_upload'
-
-    dbx_path = Column(String, primary_key=True)
+    artist = Column(String)
+    album = Column(String)
+    album_artist = Column(String)
+    song = Column(String)
+    track_number = Column(Integer)
     insertion_time = Column(DateTime)
--- audioutils/dropbox.py
+++ audioutils/dropbox.py
@@ -7,6 +7,88 @@ from dropbox.files import (
     FileMetadata, 
     FolderMetadata
 )
+from audioutils.db.tables import (
+    IncompleteDropboxDownload,
+    FailedDropboxUpload
+    IncompleteDropboxDownload,
+    FailedDropboxDownload,
+    Registry,
+    TooLargeDropboxUpload
+)
+from audioutils.db.utils import (
+    get_safe_load,
+    insert_if_not_exists
+)
+
+
+MAX_MEGABYTES = 150
+
+
+dropbox_upload_file = get_safe_load(
+    IncompleteDropboxUpload,
+    FailedDropboxUpload
+)
+
+
+def unsafe_dropbox_upload_file(dbx, local_path, get_session, media_dir):
+
+    lmd = len(media_dir) 
+    file_size = os.stat(local_path).st_size
+    megabytes = size / 10**6
+
+    row = {
+        'local_path': local_path,
+        'insertion_time': datetime.datetime.now()
+    }
+
+    if megabytes > MAX_MEGABYTES:
+        error_message = 'FILE {} EXCEEDS MAX REQUEST SIZE WITH {}MB.'.format(
+            lp, 
+            megabytes
+        )
+        row['size'] = file_size
+
+        with get_session() as session:
+            insert_if_not_exists(
+                session,
+                TooLargeDropboxUpload,
+                **row
+            )
+
+        raise Exception(error_message)
+    else:
+        with open(local_path, as 'rb') as f:
+            dbx.files_upload(
+                f,
+                local_path[lmd:]
+            )
+
+
+dropbox_download_file = 
+
+
+def unsafe_dropbox_download_file(dbx, dbx_path, get_session, media_dir):
+
+    row = {
+        'dbx_path': dbx_path,
+        'insertion_time': datetime.datetime.now()
+    }
+
+    print('DOWNLOADING FILE:', dbx_path)
+
+    (_, response) = dbx.files_download_to_file(
+        os.path.join(media_dir, dbx_path[1:]),
+        dbx_path
+    )
+    # TODO: probably raise exception if this goes wrong; need to learn about these status codes
+    print('RESPONSE STATUS CODE:', response.status_code)
+    print('RESPONSE TEXT:', response.text)
+
+    # TODO: this needs to pull out music file metadata and accordingly populate db row
+    if is_music_file:
+
+        with get_session() as session:
+            session.query(Registry).insert(**row)
 
 
 def get_remote_only_files(dbx, media_dir, dbx_dir):
